e91470671f93447431451828eddc65a12309dead
Niki <nikidwijananto@gmail.com>
2020-10-28 00:56:11 +0700

    Update 281020

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -64,7 +64,7 @@ class Auth extends CI_Controller
         $this->form_validation->set_rules('jk', 'Gander', 'required|trim');
         $this->form_validation->set_rules('alamat', 'Alamat', 'required|trim');
         $this->form_validation->set_rules('id_provinsi', 'Provinsi', 'required|trim');
-        // $this->form_validation->set_rules('foto_ktp', 'Foto KTP', 'required|trim');
+        // $this->form_validation->set_rules('foto_ktp', 'Foto KTP', 'required');
         $this->form_validation->set_rules('password', 'Password', 'required|matches[password]|min_length[4]');
         $this->form_validation->set_rules('retype_password', 'Password Confirmation', 'required|matches[password]');
 
@@ -75,14 +75,10 @@ class Auth extends CI_Controller
         } else {
             $post = $this->input->post(NULL, TRUE);
 
-            var_dump($post);
-
-            die();
-
             $config['upload_path']          = './uploads/ktp_user/';
             $config['allowed_types']        = 'jpg|jpeg|png';
             $config['max_size']             = 2048;
-            $config['file_name']            = 'KTP-' . date('Y-m-d') . '-' . substr(md5(rand()), 0, 10);
+            $config['file_name']            = 'KTP-' . indo_date(date('Y-m-d')) . '-' . substr(md5(rand()), 0, 10);
             $this->load->library('upload', $config);
 
             if ($this->upload->do_upload('foto_ktp')) {
@@ -98,11 +94,6 @@ class Auth extends CI_Controller
                 $params['level']                = 2;
                 $params['date_user']            = date('Y-m-d');
 
-
-                var_dump($params['foto_ktp']);
-                die();
-
-
                 $this->db->insert('user', $params);
 
                 $this->session->set_flashdata('message', '<div class="alert alert-success"><strong>Success!</strong> Data berhasil disimpan</div>');
@@ -110,7 +101,7 @@ class Auth extends CI_Controller
             } else {
                 $error = $this->upload->display_errors();
                 $this->session->set_flashdata('message', $error);
-                redirect('Auth');
+                redirect('v_register');
             }
         }
     }
--- application/views/v_register.php
+++ application/views/v_register.php
@@ -31,58 +31,57 @@
 
         <div class="register-box-body">
             <p class="login-box-msg">Register a new membership</p>
-            <form action="<?= site_url('Auth/register') ?>" method="post" enctype="multipart/form-data" accept-charset="utf-8">
-                <div class="form-group has-feedback <?= form_error('nama_lengkap') != NULL ? 'has-error' : null ?>">
-                    <input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap" placeholder="Nama Lengkap" value="<?= set_value('nama_lengkap') ?>" autocomplete="off" autofocus>
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    <?= form_error('nama_lengkap', '<div class="text-danger">', '</div>'); ?>
+            <?php echo form_open_multipart('Auth/register'); ?>
+            <div class="form-group has-feedback <?= form_error('nama_lengkap') != NULL ? 'has-error' : null ?>">
+                <input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap" placeholder="Nama Lengkap" value="<?= set_value('nama_lengkap') ?>" autocomplete="off" autofocus>
+                <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                <?= form_error('nama_lengkap', '<div class="text-danger">', '</div>'); ?>
+            </div>
+            <div class="form-group has-feedback <?= form_error('no_tlp') != NULL ? 'has-error' : null ?>">
+                <input type="number" id="no_tlp" name="no_tlp" pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==13) return false;" class="form-control" placeholder="No Telephone" value="<?= set_value('no_tlp') ?>" autocomplete="off">
+                <span class="glyphicon glyphicon-earphone form-control-feedback"></span>
+                <?= form_error('no_tlp', '<div class="text-danger">', '</div>'); ?>
+            </div>
+            <div class="form-group has-feedback <?= form_error('jk') != NULL ? 'has-error' : null ?>">
+                <select id="jk" name="jk" class="form-control select2_jk">
+                    <option value="">-- Pilih Jenis Kelamin --</option>
+                    <option value="1" <?= set_value('jk') == 1 ? 'selected' : null ?>>Laki-Laki</option>
+                    <option value="2" <?= set_value('jk') == 2 ? 'selected' : null ?>>Perempuan</option>
+                </select>
+                <?= form_error('jk', '<div class="text-danger">', '</div>'); ?>
+            </div>
+            <div class="form-group has-feedback <?= form_error('alamat') != NULL ? 'has-error' : null ?>">
+                <textarea id="alamat" name="alamat" cols="30" rows="4" class="form-control" placeholder="Alamat Rumah Saat Ini" autocomplete="off"><?= set_value('alamat') ?></textarea>
+                <span class="glyphicon glyphicon-home form-control-feedback"></span>
+                <?= form_error('alamat', '<div class="text-danger">', '</div>'); ?>
+            </div>
+            <div class="form-group <?= form_error('id_provinsi') != NULL ? 'has-error' : null ?>">
+                <select id="id_provinsi" name="id_provinsi" class="form-control select2">
+                    <option value="">-- Pilih Provinsi --</option>
+                    <?php foreach ($row as $key => $data) { ?>
+                        <option value="<?= $data->id_provinsi ?>" <?= set_value('id_provinsi') == $data->id_provinsi ? "selected" : null ?>><?= $data->nama_provinsi ?></option>
+                    <?php } ?>
+                </select>
+                <?= form_error('id_provinsi', '<div class="text-danger">', '</div>'); ?>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="file" id="foto_ktp" name="foto_ktp" class="form-control" placeholder="File Ktp" autocomplete="off" required>
+                <span class="glyphicon glyphicon-cloud-upload form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback <?= form_error('password') != NULL ? 'has-error' : null ?>">
+                <input type="password" name="password" class="form-control" placeholder="Password">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                <?= form_error('password', '<div class="text-danger">', '</div>'); ?>
+            </div>
+            <div class="form-group has-feedback <?= form_error('password') != NULL ? 'has-error' : null ?>">
+                <input type="password" name="retype_password" class="form-control" placeholder="Ulangi password">
+                <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
                 </div>
-                <div class="form-group has-feedback <?= form_error('no_tlp') != NULL ? 'has-error' : null ?>">
-                    <input type="number" id="no_tlp" name="no_tlp" pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==13) return false;" class="form-control" placeholder="No Telephone" value="<?= set_value('no_tlp') ?>" autocomplete="off">
-                    <span class="glyphicon glyphicon-earphone form-control-feedback"></span>
-                    <?= form_error('no_tlp', '<div class="text-danger">', '</div>'); ?>
-                </div>
-                <div class="form-group has-feedback <?= form_error('jk') != NULL ? 'has-error' : null ?>">
-                    <select id="jk" name="jk" class="form-control select2_jk">
-                        <option value="">-- Pilih Jenis Kelamin --</option>
-                        <option value="1" <?= set_value('jk') == 1 ? 'selected' : null ?>>Laki-Laki</option>
-                        <option value="2" <?= set_value('jk') == 2 ? 'selected' : null ?>>Perempuan</option>
-                    </select>
-                    <?= form_error('jk', '<div class="text-danger">', '</div>'); ?>
-                </div>
-                <div class="form-group has-feedback <?= form_error('alamat') != NULL ? 'has-error' : null ?>">
-                    <textarea id="alamat" name="alamat" cols="30" rows="4" class="form-control" placeholder="Alamat Rumah Saat Ini" autocomplete="off"><?= set_value('alamat') ?></textarea>
-                    <span class="glyphicon glyphicon-home form-control-feedback"></span>
-                    <?= form_error('alamat', '<div class="text-danger">', '</div>'); ?>
-                </div>
-                <div class="form-group <?= form_error('id_provinsi') != NULL ? 'has-error' : null ?>">
-                    <select id="id_provinsi" name="id_provinsi" class="form-control select2">
-                        <option value="">-- Pilih Provinsi --</option>
-                        <?php foreach ($row as $key => $data) { ?>
-                            <option value="<?= $data->id_provinsi ?>" <?= set_value('id_provinsi') == $data->id_provinsi ? "selected" : null ?>><?= $data->nama_provinsi ?></option>
-                        <?php } ?>
-                    </select>
-                    <?= form_error('id_provinsi', '<div class="text-danger">', '</div>'); ?>
-                </div>
-                <div class="form-group has-feedback <?= form_error('foto_ktp') != NULL ? 'has-error' : null ?>">
-                    <input id="foto_ktp" name="foto_ktp" type="file" class="form-control" placeholder="File Ktp" value="<?= set_value('foto_ktp') ?>" autocomplete="off">
-                    <span class="glyphicon glyphicon-cloud-upload form-control-feedback"></span>
-                    <?= form_error('foto_ktp', '<div class="text-danger">', '</div>'); ?>
-                </div>
-                <div class="form-group has-feedback <?= form_error('password') != NULL ? 'has-error' : null ?>">
-                    <input type="password" name="password" class="form-control" placeholder="Password">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    <?= form_error('password', '<div class="text-danger">', '</div>'); ?>
-                </div>
-                <div class="form-group has-feedback <?= form_error('password') != NULL ? 'has-error' : null ?>">
-                    <input type="password" name="retype_password" class="form-control" placeholder="Ulangi password">
-                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-                </div>
-                <div class="row">
-                    <div class="col-xs-12">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
-                    </div>
-                </div>
+            </div>
             </form>
             <hr>
             <a href="<?= base_url('Auth') ?>" class="text-center">I already have a membership</a>
